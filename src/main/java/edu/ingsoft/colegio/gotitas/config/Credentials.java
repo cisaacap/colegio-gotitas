fc573d0b83542818c91eb62ecbad86011a9b979e
NeryCoy <ncoy-2026162@kinal.edu.gt>
2026-09-04 11:25:49 -0600

    Update Credentials.java

--- src/main/java/edu/ingsoft/colegio/gotitas/config/Credentials.java
+++ src/main/java/edu/ingsoft/colegio/gotitas/config/Credentials.java
@@ -3,10 +3,10 @@ package main.java.edu.ingsoft.colegio.gotitas.config;
 //nunca exponer credenciales en github
 public class Credentials {
     
-    public static final String DATA_BASE = ("colegio_gotitas_del_saber_in4bm");
-    public static final String URL_DB = ("jdbc:mysql://localhost:3306/colegio_gotitas_del_saber_in4bm");
-    public static final String PASS_DB = ("$DmynM4A");
-    public static final String USER_DB = ("IN4BM");
+    public static final String DATA_BASE = System.getenv("DATA_BASE");
+    public static final String URL_DB = System.getenv("URL_MYSQL_DB")+DATA_BASE;
+    public static final String PASS_DB = System.getenv("PASS_MYSQL_DB");
+    public static final String USER_DB = System.getenv("USER_MYSQL_DB");
     
 }
 
